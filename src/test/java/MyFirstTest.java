002e877dba6a80e3d3a6faaf5dc7f3bb27f442f0
manintech <>
2021-10-28 04:42:04 -0700

    Part01. Learning Java essentials with Maven and TestNG

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -4,14 +4,15 @@ public class MyFirstTest {
 
     @Test
     public void testHello() {
-        System.out.println("Hello World!");
+        System.out.println("Hello, World!");
     }
 
     @Test
-    public void testNamber() {
-        int myNamber;
-        myNamber = 5;
-        System.out.println(myNamber);
+    public void testNambers() {
+        int namberOne = 55;
+        int namberTwo = 77;
+        System.out.println(namberOne + namberTwo);
+        System.out.println(namberOne + " " + namberTwo);
     }
 
     @Test
@@ -28,7 +29,54 @@ public class MyFirstTest {
         int numberOfClicks = 5;
         System.out.println(messgPrefix + numberOfClicks + messgSuffix);
     }
+
+    @Test
+    public void testMyMethod01() {
+        int firstNumber = 5;
+        int secondNumber = 6;
+        System.out.println("First number: " + firstNumber + "\n" + "Second number: " + secondNumber);
+    }
+
+    public void printInputValue(int firstNumber, int secondNumber){
+        String messagePrefix = "First number is: ";
+        String messagePostfix = "Second number is: ";
+        System.out.println(messagePrefix + firstNumber + "\n" + messagePostfix + secondNumber);
+    }
+
+    @Test
+    public void testMyMethod02() {
+        printInputValue(5, 6);
+    }
+
+    public void printLoginSteps(String loginValue, String passwordValue){
+        System.out.println("I typed login: " + loginValue);
+        System.out.println("I typed password: " + passwordValue);
+        System.out.println("I clicked login button");
+    }
+
+    @Test
+    public void testLoginTest() {
+        printLoginSteps("joeblack", "SuperSecretPassw123");
+    }
+
+    @Test
+    public void testArrays() {
+        int[] arrayOfInts = {1, 2, 3, 4, 5};
+        arrayOfInts[2] = 4;
+        System.out.println(arrayOfInts[2]);
+    }
+
+    @Test
+    public void testForLoop() {
+        int[] arrayOfInts = {1, 2, 3, 4, 5};
+
+        for (int i = 1; i < arrayOfInts.length - 1; i++){
+            int currentValue = arrayOfInts[i];
+            System.out.println(currentValue);
+        }
+    }
 }
+
 
 
 
