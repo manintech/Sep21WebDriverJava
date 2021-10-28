ed523d1e5fc1fc87992717e1bedc9c02643ffa59
manintech <>
2021-10-27 19:05:42 -0700

    A litle more tests

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -3,8 +3,32 @@ import org.testng.annotations.Test;
 public class MyFirstTest {
 
     @Test
-    public void testName() {
+    public void testHello() {
         System.out.println("Hello World!");
     }
 
+    @Test
+    public void testNamber() {
+        int myNamber;
+        myNamber = 5;
+        System.out.println(myNamber);
+    }
+
+    @Test
+    public void testConcatenation() {
+        String s1 = "Dogs are!";
+        String s2 = "Who! Who! Who!";
+        System.out.println(s1 + ' ' + s2);
+    }
+
+    @Test
+    public void testClicks() {
+        String messgPrefix ="I clicked ";
+        String messgSuffix = " times!";
+        int numberOfClicks = 5;
+        System.out.println(messgPrefix + numberOfClicks + messgSuffix);
+    }
 }
+
+
+
